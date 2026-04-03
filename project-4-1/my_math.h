a7b8f78133551cf347da3f4967c41c5b85e00f42
stroplezero <a01039072925@gmail.com>
2026-04-03 18:42:59 +0900

    Create my_math.h

---
+++ project-4-1/my_math.h
@@ -0,0 +1,7 @@
+typedef unsigned int my_math;
+
+//두 정수(i, j)를 입력받으면 유클리드 호제법(Euclidean Algorithm)으로 최대공약수(GCD)를 구함
+extern my_math get_gcd(my_math i, my_math j);
+
+//하나의 정수(n)을 입력받으면 피보나치 수열에서 n번째 자리의 수를 구함.
+extern my_math F(my_math n);
