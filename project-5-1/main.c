a6c88039e4b79e5389e04b092353fe37aa3e70da
stroplezero <a01039072925@gmail.com>
2026-04-05 10:46:02 +0900

    Create main.c

---
+++ project-5-1/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define SIZE 10
+
+int main() {
+
+	srand((unsigned)time(NULL));
+
+	int iArray[SIZE];
+
+	for (int i = 0; i < SIZE; i++) {
+		iArray[i] = rand() % (SIZE + 1);
+		printf("%d ", iArray[i]);
+	}
+	printf("\n");
+
+	for (int i = SIZE - 1; i >= 0; i--) {
+		printf("%d ", iArray[i]);
+	}
+	printf("\n");
+
+	int total = 0;
+
+	for (int i = 0; i < SIZE; i++) {
+		total = total + iArray[i];
+	}
+	printf("total: %d\n", total);
+
+	int max = iArray[0];
+
+	for (int i = 0; i < SIZE; i++) {
+		if (max < iArray[i]) {
+			max = iArray[i];
+		}
+	}
+	printf("max: %d\n", max);
+
+	int min = iArray[0];
+	
+	for (int i = 0; i < SIZE; i++) {
+		if (min > iArray[i]) {
+			min = iArray[i];
+		}
+	}
+	printf("min: %d\n", min);
+
+	return 0;
+}
