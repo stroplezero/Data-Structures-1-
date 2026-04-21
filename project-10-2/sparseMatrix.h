8b5af8170bb2f4f3a92dfe18cb07f31449ae980c
stroplezero <a01039072925@gmail.com>
2026-04-21 23:10:10 +0900

    Update sparseMatrix.h

--- project-10-2/sparseMatrix.h
+++ project-10-2/sparseMatrix.h
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+#include "arrayList.h"
+
+typedef struct sparseMatrixType {
+	int row;
+	int col;
+	arrayList* value;
+} sparseMatrix;
+
+extern sparseMatrix* createSparseMatrix(int row, int col);
+extern int addElementSparseMatrix(sparseMatrix* sm, int row, int col, int value);
+extern int printSparseMatrix(sparseMatrix* sm);
+extern sparseMatrix* addSparseMatrix(sparseMatrix* sm1, sparseMatrix* sm2);
+extern sparseMatrix* transposeMatrix(sparseMatrix* sm);
