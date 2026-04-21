89e7567671c9c3633f99d10033b9953f76e47367
stroplezero <a01039072925@gmail.com>
2026-04-21 23:12:34 +0900

    Update main.c

--- project-10-2/main.c
+++ project-10-2/main.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+#include "sparseMatrix.h"
+
+int main() {
+
+	int row = 10;
+	int col = 10;
+
+	sparseMatrix* sm = createSparseMatrix(row, col);
+
+	int k = 0; 
+
+	for (int i = 0; i < row && k < 20; i++) {
+		for (int j = 0; j < col && k < 20; j++) {
+			addElementSparseMatrix(sm, i, j, (i + j + 1) % 10);
+
+			k++;
+		}
+	}
+
+	printSparseMatrix(sm);
+	printf("\n");
+
+	sparseMatrix* transposeSM = transposeMatrix(sm);
+
+	printf("전치 ");
+	printSparseMatrix(transposeSM);
+	printf("\n");
+	
+	return 0;
+}
