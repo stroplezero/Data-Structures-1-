521a244b053ab9c5712406d0924eef04d594ca41
stroplezero <a01039072925@gmail.com>
2026-04-21 23:10:48 +0900

    Add array list data structure and functions

--- project-10-2/arrayList.h
+++ project-10-2/arrayList.h
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+typedef struct elementArrayListType {
+	int row;
+	int col;
+	int value;
+} elementArrayList;
+
+typedef struct arrayListType {
+	elementArrayList* data;
+	int size;
+	int capacity;
+} arrayList;
+
+extern arrayList* createArrayList(int size);
+extern void destroyArrayList(arrayList* al);
+extern int isEmptyArrayList(arrayList* al);
+extern int isFullArrayList(arrayList* al);
+extern int sizeArrayList(arrayList* al);
+extern int insertArrayList(arrayList* al, int pos, elementArrayList item);
+extern elementArrayList deleteArrayList(arrayList* al, int pos);
+extern int initArrayList(arrayList* al);
+extern elementArrayList getItemArrayList(arrayList* al, int pos);
+extern int replaceItemArrayList(arrayList* al, int pos, elementArrayList item);
+extern void printArrayList(arrayList* al);
+extern void clearArrayList(arrayList* al);
