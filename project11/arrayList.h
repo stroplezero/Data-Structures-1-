c4eb858806e4f10fa96b8f8b6e15d3ace5d0cf80
stroplezero <a01039072925@gmail.com>
2026-05-03 17:51:19 +0900

    Create arrayList.h

---
+++ project11/arrayList.h
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct ElementType {
+	int coef;
+	int expo;
+} elementType;
+
+typedef elementType elementArrayList;
+
+typedef struct arrayListType {
+	elementArrayList* data;
+	int size;
+	int capacity;
+} arrayList;
+
+extern arrayList* createArrayList(int size);
+extern void destroyArrayList(arrayList* al);
+extern int isEmptyArrayList(arrayList* al);
+extern int isFullArrayList(arrayList* al);
+extern int sizeArrayList(arrayList* al);
+extern int insertArrayList(arrayList* al,int pos, elementArrayList item);
+extern elementArrayList deleteArrayList(arrayList* al, int pos);
+extern int initArrayList(arrayList* al);
+extern elementArrayList getItemArrayList(arrayList* al, int pos);
+extern int replaceItemArrayList(arrayList* al,int pos, elementArrayList item);
+extern void printArrayList(arrayList* al);
