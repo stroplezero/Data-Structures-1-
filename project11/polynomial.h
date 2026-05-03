f8c4a8d9ecd85f822ea850cefe310be6562a2962
stroplezero <a01039072925@gmail.com>
2026-05-03 17:50:58 +0900

    Create polynomial.h

---
+++ project11/polynomial.h
@@ -0,0 +1,10 @@
+#include "arrayList.h"
+
+typedef arrayList polynomial;
+
+extern polynomial* createPolynomial(int capacity);
+extern polynomial* addItemPolynomial(polynomial* po, elementType item);
+extern void printPolynomial(polynomial* po);
+extern polynomial* addPolynomial(polynomial* p1, polynomial* p2);
+extern polynomial* inputPolynomial(po);
+extern polynomial* multiPolynomial(polynomial* p1, polynomial* p2);
