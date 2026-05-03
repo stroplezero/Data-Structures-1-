390f477854ae5b0478aadb2f7b6bcb49f2d6e4c1
stroplezero <a01039072925@gmail.com>
2026-05-03 17:53:47 +0900

    Update polynomial.h

--- project11/polynomial.h
+++ project11/polynomial.h
@@ -6,5 +6,5 @@ extern polynomial* createPolynomial(int capacity);
 extern polynomial* addItemPolynomial(polynomial* po, elementType item);
 extern void printPolynomial(polynomial* po);
 extern polynomial* addPolynomial(polynomial* p1, polynomial* p2);
-extern polynomial* inputPolynomial(po);
+extern polynomial* inputPolynomial(polynomial* po);
 extern polynomial* multiPolynomial(polynomial* p1, polynomial* p2);
