a706f23bd92451885eb59baa8a8b2ca5a83b6799
stroplezero <a01039072925@gmail.com>
2026-05-03 17:51:37 +0900

    Create arrayList.c

---
+++ project11/arrayList.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "arrayList.h"
+
+arrayList* createArrayList(int size) {
+	arrayList* al;
+	al = (arrayList*)malloc(sizeof(arrayList));
+
+	al->data = (elementArrayList*)malloc(sizeof(elementArrayList) * size);
+	al->size = 0;
+	al->capacity = size;
+
+	return al;
+}
+
+void destroyArrayList(arrayList* al) {
+	free(al->data);
+	free(al);
+}
+
+int isEmptyArrayList(arrayList* al) {
+	if (al->size == 0) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int isFullArrayList(arrayList* al) {
+	if (al->size == al->capacity) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int sizeArrayList(arrayList* al) {
+	return al->size;
+}
+
+int insertArrayList(arrayList* al,
+	int pos, elementArrayList item) {
+	if (pos < 0 || pos > al->size) {
+		return 0;
+	}
+
+	for (int i = al->size; i >= pos; i--) {
+		al->data[i + 1] = al->data[i];
+	}
+
+	al->data[pos] = item;
+	al->size++;
+
+	return 1;
+}
+
+elementArrayList deleteArrayList(arrayList* al, int pos) {
+	if (pos < 0 || pos > al->size - 1) {
+		return (elementArrayList) { 0, 0 };
+	}
+
+	elementArrayList item = al->data[pos];
+
+	for (int i = pos; i < al->size - 1; i++) {
+		al->data[i] = al->data[i + 1];
+	}
+
+	al->size--;
+
+	return item;
+}
+
+int initArrayList(arrayList* al) {
+	for (int i = al->size - 1; i >= 0; i--) {
+		deleteArrayList(al, i);
+	}
+}
+
+elementArrayList getItemArrayList(
+	arrayList* al, int pos) {
+	return al->data[pos];
+}
+
+int replaceItemArrayList(arrayList* al,
+	int pos, elementArrayList item) {
+	if (pos < 0 || pos > al->size - 1) {
+		return 0;
+	}
+
+	al->data[pos] = item;
+
+	return 1;
+}
+
+void printArrayList(arrayList* al) {
+	printf("Array List: ");
+
+	for (int i = 0; i < al->size; i++) {
+		printf("(%d %d)", al->data[i].coef,
+			al->data[i].expo);
+	}
+}
