6e1a94d44169a17a66e540fcde43aaa38230fa1c
stroplezero <a01039072925@gmail.com>
2026-05-12 10:32:49 +0900

    Create LinkedList.h

---
+++ project-12/LinkedList.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct pointType {
+	int x;
+	int y;
+	struct pointType* next;
+} PointType;
+
+typedef struct linkedList {
+	struct pointType* head;
+	int size;
+} LinkedList;
+
+extern LinkedList* createLinkedList();
+extern int destroyLinkedList(LinkedList* li);
+extern int isEmptyLinkedList(LinkedList* li);
+extern int sizeLinkedList(LinkedList* li);
+extern LinkedList* insertFirstLinkedList(LinkedList* li, PointType item);
+extern int printLinkedList(LinkedList* li);
+extern LinkedList* insertLastLinkedList(LinkedList* li, PointType item);
+extern LinkedList* deleteAtLinkedList(LinkedList* li, int at);
+extern LinkedList* insertItemLinkedList(LinkedList* li, PointType* pre, PointType item);
+extern PointType deleteFirstLinkedList(LinkedList* li);
+extern PointType deleteLastLinkedList(LinkedList* li);
